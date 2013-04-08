33bb544a9c872a0fdae648b7d6bb8d7ae14cc208
Gareth Flowers <garethflowers@users.noreply.github.com>
2013-04-08 08:40:35 +0100

    Improved PHPDoc

--- library/Twitter/View/Helper/Breadcrumb.php
+++ library/Twitter/View/Helper/Breadcrumb.php
@@ -10,10 +10,35 @@
  */
 class Twitter_View_Helper_Breadcrumb extends Zend_View_Helper_Abstract {
 
+    /**
+     * Active Title
+     *
+     * @var string
+     */
     private $_active;
+
+    /**
+     * Links
+     *
+     * @var array
+     */
     private $_links;
+
+    /**
+     * HTML Class
+     *
+     * @var string
+     */
     private $_class;
 
+    /**
+     * Creates a new Breadcrumb instance
+     *
+     * @param string $active
+     * @param array $links
+     * @param string $class
+     * @return Twitter_View_Helper_Breadcrumb
+     */
     public function breadcrumb($active = '', array $links = array(), $class = '') {
         $this->setActive($active);
         $this->setLinks($links);
@@ -22,26 +47,56 @@ class Twitter_View_Helper_Breadcrumb extends Zend_View_Helper_Abstract {
         return $this;
     }
 
+    /**
+     * Sets the title on the Active item
+     *
+     * @param string $active
+     * @return Twitter_View_Helper_Breadcrumb
+     */
     public function setActive($active) {
         $this->_active = (string) $active;
         return $this;
     }
 
+    /**
+     * Adds a Link
+     *
+     * @param string $link
+     * @param string $title
+     * @return Twitter_View_Helper_Breadcrumb
+     */
     public function addLink($link, $title) {
         $this->_links[$link] = $title;
         return $this;
     }
 
+    /**
+     * Sets the links used
+     *
+     * @param array $links
+     * @return Twitter_View_Helper_Breadcrumb
+     */
     public function setLinks(array $links) {
         $this->_links = $links;
         return $this;
     }
 
+    /**
+     * Sets the HTML Class for the Breadcrumb
+     *
+     * @param string $class
+     * @return Twitter_View_Helper_Breadcrumb
+     */
     public function setClass($class) {
         $this->_class = (string) $class;
         return $this;
     }
 
+    /**
+     * Renders the Breadcrumb as an HTML string
+     *
+     * @return string
+     */
     public function __toString() {
         $html = '<ul class="breadcrumb ' . $this->_class . '">';
         foreach ($this->_links as $link => $title) {
