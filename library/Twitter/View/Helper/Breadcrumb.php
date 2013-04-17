105e5779ad8067745b177793bd8dcc7a55c91f09
Gareth Flowers <garethflowers@users.noreply.github.com>
2013-04-17 09:47:38 +0100

    Breadcrumb now uses Zend_Navigation
    Helper now extends Zend_View_Helper_Navigation_Breadcrumbs and must be
    passed a valid Zend_Navigation_Container to render correctly.

--- library/Twitter/View/Helper/Breadcrumb.php
+++ library/Twitter/View/Helper/Breadcrumb.php
@@ -8,109 +8,75 @@
  * @subpackage Helper_Breadcrumb
  * @copyright Gareth Flowers
  */
-class Twitter_View_Helper_Breadcrumb extends Zend_View_Helper_Abstract {
+class Twitter_View_Helper_Breadcrumb extends Zend_View_Helper_Navigation_Breadcrumbs {
 
-    /**
-     * Active Title
-     *
-     * @var string
-     */
-    private $_active;
+    protected $_class;
+    protected $_urlPrefix;
 
-    /**
-     * Links
-     *
-     * @var array
-     */
-    private $_links;
-
-    /**
-     * HTML Class
-     *
-     * @var string
-     */
-    private $_class;
-
-    /**
-     * Creates a new Breadcrumb instance
-     *
-     * @param string $active
-     * @param array $links
-     * @param string $class
-     * @return Twitter_View_Helper_Breadcrumb
-     */
-    public function breadcrumb($active = '', array $links = array(), $class = '') {
-        $this->setActive($active);
-        $this->setLinks($links);
-        $this->setClass($class);
-
-        return $this;
+    public function breadcrumb(Zend_Navigation_Container $container = null) {
+        $this->setSeparator('/');
+        return parent::breadcrumbs($container);
     }
 
-    /**
-     * Sets the title on the Active item
-     *
-     * @param string $active
-     * @return Twitter_View_Helper_Breadcrumb
-     */
-    public function setActive($active) {
-        $this->_active = (string) $active;
-        return $this;
-    }
-
-    /**
-     * Adds a Link
-     *
-     * @param string $link
-     * @param string $title
-     * @return Twitter_View_Helper_Breadcrumb
-     */
-    public function addLink($link, $title) {
-        $this->_links[$link] = $title;
-        return $this;
-    }
-
-    /**
-     * Sets the links used
-     *
-     * @param array $links
-     * @return Twitter_View_Helper_Breadcrumb
-     */
-    public function setLinks(array $links) {
-        $this->_links = $links;
-        return $this;
-    }
-
-    /**
-     * Sets the HTML Class for the Breadcrumb
-     *
-     * @param string $class
-     * @return Twitter_View_Helper_Breadcrumb
-     */
     public function setClass($class) {
-        $this->_class = (string) $class;
+        if (is_string($class)) {
+            $this->_class = $class;
+        }
+
         return $this;
     }
 
-    /**
-     * Renders the Breadcrumb as an HTML string
-     *
-     * @return string
-     */
-    public function __toString() {
-        $html = '<ul class="breadcrumb ' . $this->_class . '">';
-        foreach ($this->_links as $link => $title) {
-            $html .= '<li>';
-            $html .= $this->view->urlLink($link, $title);
-            $html .= '<span class="divider">/</span>';
-            $html .= '</li>';
+    public function setUrlPrefix($prefix) {
+        if (is_string($prefix)) {
+            $this->_urlPrefix = $prefix;
         }
-        $html .= '<li class="active">';
-        $html .= $this->_active;
-        $html .= '</li>';
-        $html .= '</ul>';
 
-        return $html;
+        return $this;
+    }
+
+    public function renderStraight(Zend_Navigation_Container $container = null) {
+        if (null === $container) {
+            $container = $this->getContainer();
+        }
+
+        // find deepest active
+        if (!$active = $this->findActive($container)) {
+            return '';
+        }
+
+        $active = $active['page'];
+
+        // put the deepest active page last in breadcrumbs
+        if ($this->getLinkLast()) {
+            $html = ' <li>' . $this->htmlify($active) . '</li>' . PHP_EOL;
+        } else {
+            $html = $active->getLabel();
+            if ($this->getUseTranslator() && $t = $this->getTranslator()) {
+                $html = $t->translate($html);
+            }
+            $html = ' <li class="active">' . $this->view->escape($html) .
+                    '</li>' . PHP_EOL;
+        }
+
+        // walk back to root
+        while (($parent = $active->getParent()) != null) {
+            if ($parent instanceof Zend_Navigation_Page) {
+                // prepend crumb to html
+                $html = ' <li>' . $this->htmlify($parent) .
+                        ' <span class="divider">' . $this->getSeparator() .
+                        '</span></li>' . PHP_EOL . $html;
+            }
+
+            if ($parent === $container) {
+                // at the root of the given container
+                break;
+            }
+
+            $active = $parent;
+        }
+
+        return strlen($html) ? $this->getIndent() . '<ul class="breadcrumb ' .
+                $this->_class . '">' . PHP_EOL . $html . '</ul>' . PHP_EOL : '';
     }
 
 }
