215e0fc019d39fd8e053c958487fdb2a666b887c
Gareth Flowers <garethflowers@users.noreply.github.com>
2014-01-01 19:21:01 +0000

    Updated for Twitter Bootstrap 3

--- library/Twitter/Form.php
+++ library/Twitter/Form.php
@@ -9,17 +9,49 @@
  */
 class Twitter_Form extends Zend_Form {
 
+    const TYPE_HORIZONTAL = 'horizontal';
+    const TYPE_INLINE = 'inline';
+
+    /**
+     * Form Type
+     * @var string
+     */
+    private $_type;
+
     public function __construct($options = null) {
-        // clear existing decorators
-        $this->clearDecorators();
+        $this->setName(strtolower(get_called_class()));
+        $this->setAttrib('role', 'form');
 
-        // Decorators for the form itself
-        $this->setDecorators(array('FormElements'));
+        $iframeid = $this->getName() . '_results';
+        $this->setAttrib('target', $iframeid);
 
-        // Decorators for all the form elements
-        $this->setElementDecorators($this->_getElementDecorators());
+        $decorator = array('FormElements');
+        $decorator[] = array(array('Results-Frame' => 'HtmlTag'),
+            array('tag' => 'iframe', 'id' => $iframeid, 'class' => 'hidden', 'placement' => 'append'));
+        $decorator[] = 'Form';
+        $this->setDecorators($decorator);
 
         parent::__construct($options);
+    }
+
+    /**
+     * Set the Form Type to Horizontal
+     * @param bool $flag
+     * @return Twitter_Form
+     */
+    public function setFormTypeHorizontal($flag = true) {
+        $this->_type = (bool) $flag ? self::TYPE_HORIZONTAL : null;
+        return $this;
+    }
+
+    /**
+     * Set the Form Type to Inline
+     * @param bool $flag
+     * @return Twitter_Form
+     */
+    public function setFormTypeInline($flag = true) {
+        $this->_type = (bool) $flag ? self::TYPE_INLINE : null;
+        return $this;
     }
 
     /**
@@ -28,25 +60,8 @@ class Twitter_Form extends Zend_Form {
      * @return Zend_View
      */
     public function render(Zend_View_Interface $view = null) {
-        $formTypes = array(
-            'horizontal',
-            'inline',
-            'vertical',
-            'search'
-        );
-
-        $set = false;
-
-        foreach ($formTypes as $type) {
-            if ($this->getAttrib($type)) {
-                $this->removeAttrib($type);
-                $this->addDecorator('Form', array('class' => 'form-' . $type));
-                $set = true;
-            }
-        }
-
-        if ($set !== true) {
-            $this->addDecorator('Form', array('class' => 'form-horizontal'));
+        if (!empty($this->_type)) {
+            $this->setAttrib('class', trim('form-' . $this->_type . ' ' . $this->getAttrib('class')));
         }
 
         return parent::render($view);
@@ -54,7 +69,7 @@ class Twitter_Form extends Zend_Form {
 
     /**
      * @see Zend_Form::addDisplayGroup
-     *
+     * 
      * @param array $elements
      * @param string $name
      * @param array|Zend_Config $options
@@ -62,7 +77,7 @@ class Twitter_Form extends Zend_Form {
     public function addDisplayGroup(array $elements, $name = null, $options = null) {
         parent::addDisplayGroup($elements, $name, $options);
 
-        if ($name != null && $name !== 'zfBootstrapFormActions') {
+        if ($name != null && $name !== 'form-group-actions') {
             $displayGroup = $this->getDisplayGroup($name);
 
             if ($displayGroup instanceof Zend_Form_DisplayGroup) {
@@ -70,6 +85,8 @@ class Twitter_Form extends Zend_Form {
                 $displayGroup->removeDecorator('HtmlTag');
             }
         }
+
+        return $this;
     }
 
     /**
@@ -84,52 +101,40 @@ class Twitter_Form extends Zend_Form {
      * @param array|Zend_Config $options
      */
     public function addElement($element, $name = null, $options = null) {
+        $element->setDisableLoadDefaultDecorators(true);
+
         parent::addElement($element, $name, $options);
 
-        if (!$element instanceof Zend_Form_Element && $name != null) {
+        if (!$element instanceof Zend_Form_Element && $name !== null) {
             $element = $this->getElement($name);
         } else {
-            $element->clearDecorators();
-            $element->setDecorators($this->_getElementDecorators());
+            $element->setDecorators($this->_getDefaultElementDecorator($element));
         }
 
         if ($element instanceof Zend_Form_Element_File) {
-            $decorators = $this->_getElementDecorators();
-            $decorators[0] = 'File';
-            $element->setDecorators($decorators);
+            $this->setAttrib('enctype', Zend_Form::ENCTYPE_MULTIPART);
+
+            if (!$element->getDescription()) {
+                $element->setDescription('Add File...');
+            }
         }
 
-        // Special style for Zend
         if ($element instanceof Zend_Form_Element_Submit || $element instanceof Zend_Form_Element_Reset || $element instanceof Zend_Form_Element_Button) {
-            $class = '';
-
-            if ($element instanceof Zend_Form_Element_Submit && !$element instanceof Zend_Form_Element_Reset && !$element instanceof Zend_Form_Element_Button) {
+            if (!$element instanceof Zend_Form_Element_Reset && !$element instanceof Zend_Form_Element_Button) {
                 $class = 'btn-primary';
+            } else if ($element instanceof Zend_Form_Element_Button && $element->getAttrib('type') === 'submit') {
+                $class = 'btn-primary';
+            } else {
+                $class = 'btn-default';
             }
 
-            $element->setAttrib('class', trim('btn ' . $class . $element->getAttrib('class')));
-            $element->removeDecorator('Label');
-            $element->removeDecorator('outerwrapper');
-            $element->removeDecorator('innerwrapper');
-
+            $element->setAttrib('class', trim('btn ' . $class . ' ' . $element->getAttrib('class')));
             $this->_addActionsDisplayGroupElement($element);
+        } else {
+            $element->setAttrib('class', trim('form-control ' . $element->getAttrib('class')));
         }
 
-        if ($element instanceof Zend_Form_Element_Checkbox) {
-            $element->setDecorators(array(
-                array(array('labelopening' => 'HtmlTag'), array('tag' => 'label', 'class' => 'checkbox', 'id' => $element->getId() . '-label', 'for' => $element->getName(), 'openOnly' => true)),
-                'ViewHelper',
-                array('Checkboxlabel'),
-                array(array('labelclosing' => 'HtmlTag'), array('tag' => 'label', 'closeOnly' => true)),
-                array('Errors', array('placement' => 'append')),
-                array('Description', array('tag' => 'span', 'class' => 'help-block')),
-                array(array('innerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'controls')),
-                array(array('outerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'control-group'))
-            ));
-        }
-
-        if ($element instanceof Zend_Form_Element_Radio ||
-                $element instanceof Zend_Form_Element_MultiCheckbox) {
+        if ($element instanceof Zend_Form_Element_Radio || $element instanceof Zend_Form_Element_MultiCheckbox) {
             $multiOptions = array();
             foreach ($element->getMultiOptions() as $value => $label) {
                 $multiOptions[$value] = ' ' . $label;
@@ -152,51 +157,90 @@ class Twitter_Form extends Zend_Form {
             }
 
             $element->setOptions(array('separator' => ''));
-            $element->setDecorators(array(
-                'ViewHelper',
-                array('Errors', array('placement' => 'append')),
-                array('Description', array('tag' => 'span', 'class' => 'help-block')),
-                array(array('innerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'controls')),
-                array('Label', array('class' => 'control-label')),
-                array(array('outerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'control-group'))
-            ));
         }
 
         if ($element instanceof Zend_Form_Element_Hidden) {
             $element->setDecorators(array('ViewHelper'));
         }
 
-        if ($element instanceof Zend_Form_Element_Textarea &&
-                !$element->getAttrib('rows')) {
-            $element->setAttrib('rows', '3');
+        if ($element instanceof Zend_Form_Element_Textarea && !$element->getAttrib('rows')) {
+            $element->setAttrib('rows', 3);
         }
 
-        if ($element instanceof Zend_Form_Element_Captcha) {
-            $element->removeDecorator('viewhelper');
+        if ($element->isRequired()) {
+            $element->setAttrib('required', 'required');
         }
 
         return $this;
     }
 
-    private function _getElementDecorators() {
-        return array(
-            'ViewHelper',
-            array('Errors', array('placement' => 'append')),
-            array('Description', array('tag' => 'span', 'class' => 'help-block')),
-            array(array('innerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'controls')),
-            array('Label', array('class' => 'control-label')),
-            array(array('outerwrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'control-group'))
-        );
+    /**
+     * 
+     * @param Zend_Form_Element $element
+     * @return string[]
+     */
+    private function _getDefaultElementDecorator(Zend_Form_Element $element) {
+        $decorator = array();
+
+        if ($element instanceof Zend_Form_Element_Checkbox) {
+            $decorator[] = array(array('Label-Open' => 'HtmlTag'), array('tag' => 'label', 'class' => 'checkbox', 'id' => $element->getId() . '-label', 'for' => $element->getName(), 'openOnly' => true));
+            $decorator[] = 'ViewHelper';
+            $decorator[] = array('CheckBoxLabel');
+            $decorator[] = array(array('Label-Closing' => 'HtmlTag'), array('tag' => 'label', 'closeOnly' => true));
+            $decorator[] = array('Errors', array('placement' => 'append'));
+            $decorator[] = array('Description', array('tag' => 'span', 'class' => 'help-block'));
+            $decorator[] = array(array('Inner-Wrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'col-sm-10'));
+            $decorator[] = array(array('Outer-Wrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'form-group'));
+            return $decorator;
+        }
+
+        if ($element instanceof Zend_Form_Element_Submit || $element instanceof Zend_Form_Element_Reset || $element instanceof Zend_Form_Element_Button) {
+            $decorator[] = 'ViewHelper';
+            return $decorator;
+        }
+
+        if ($element instanceof Zend_Form_Element_File) {
+            $decorator[] = 'File';
+            $decorator[] = 'Errors';
+            $decorator[] = array('Description', array('tag' => 'span', 'class' => 'input-file', 'placement' => 'prepend'));
+            $decorator[] = array(array('File-Wrapper' => 'HtmlTag'), array('tag' => 'span', 'class' => 'btn btn-default'));
+        } else {
+            $decorator[] = 'ViewHelper';
+            $decorator[] = 'Errors';
+            $decorator[] = array('Description', array('tag' => 'span', 'class' => 'help-block'));
+        }
+
+        if ($this->_type === self::TYPE_HORIZONTAL) {
+            $decorator[] = array(array('Inner-Wrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'col-sm-10'));
+            $decorator[] = array('Label', array('class' => 'control-label col-sm-2'));
+            $decorator[] = array(array('Outer-Wrapper' => 'HtmlTag'), array('tag' => 'div', 'class' => 'form-group'));
+        } else {
+            $decorator[] = 'Label';
+            $decorator[] = array('HtmlTag', array('tag' => 'div', 'class' => 'form-group'));
+        }
+
+        return $decorator;
     }
 
-    private function _addActionsDisplayGroupElement($element) {
-        $displayGroup = $this->getDisplayGroup('zfBootstrapFormActions');
+    /**
+     * 
+     * @param Zend_Form_Element_Submit $element
+     * @return type
+     */
+    private function _addActionsDisplayGroupElement(Zend_Form_Element_Submit $element) {
+        $displayGroup = $this->getDisplayGroup('form-group-actions');
 
-        if ($displayGroup === null) {
-            $displayGroup = $this->addDisplayGroup(
-                    array($element), 'zfBootstrapFormActions', array('decorators' => array(
-                    'FormElements', array('HtmlTag', array('tag' => 'div', 'class' => 'form-actions'))))
-            );
+        if (!$displayGroup instanceof Zend_Form_DisplayGroup) {
+            $decorator = array('FormElements');
+
+            if ($this->_type === self::TYPE_HORIZONTAL) {
+                $decorator[] = array(array('Inner-Wrapper' => 'HtmlTag'),
+                    array('tag' => 'div', 'class' => 'col-sm-offset-2 col-sm-10'));
+                $decorator[] = array(array('Outer-Wrapper' => 'HtmlTag'),
+                    array('tag' => 'div', 'class' => 'form-group'));
+            }
+
+            $displayGroup = $this->addDisplayGroup(array($element), 'form-group-actions', array('decorators' => $decorator));
         } else {
             $displayGroup->addElement($element);
         }
